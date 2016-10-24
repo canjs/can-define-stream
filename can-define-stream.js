69d7719728ebad4e8f025d068c4248b6a23d8ae4
Obaid Ahmed <obaid@bitovi.com>
2016-10-24 09:17:00 -0400

    Initial implementation

--- can-define-stream.js
+++ can-define-stream.js
@@ -1,7 +1,31 @@
+var define = require('can-define');
+var compute = require('can-compute');
 var canStream = require('can-stream');
+var isArray = require("can-util/js/is-array/is-array");
+var assign = require("can-util/js/assign/assign");
 var DefineMap = require('can-define/map/map');
+
+var oldExtensions = define.extensions;
+define.behaviors.push('stream');
+
+define.extensions = function (objPrototype, prop, definition) {
+
+	if (definition.stream) {
+		return assign({
+			value: function() {
+				debugger;
+				var internalStream = definition.stream.call(objPrototype, canStream.toStream(objPrototype, prop));
+				//internalStream.onValue()
+				return internalStream;
+			}
+		}, define.types.stream);
+
+	} else {
+		return oldExtensions.apply(this, arguments);
+	}
+};
 
 DefineMap.prototype.stream = function() {
 	[].unshift.call(arguments, this);
-	return canStream.toStreamFromEvent.apply(this, arguments);
+	return canStream.toStream.apply(this, arguments);
 };
--- can-define-stream_test.js
+++ can-define-stream_test.js
@@ -1,75 +1,117 @@
 var QUnit = require('steal-qunit');
 var DefineMap = require('can-define/map/map');
 var DefineList = require('can-define/list/list');
+//toremove
+var define = require('can-define');
+var assign = require("can-util/js/assign/assign");
 require('can-define-stream');
 
 QUnit.module('can-define-stream');
 
-test('Create a stream on a property', function() {
+// test('Create a stream on a property', function() {
+//
+// 	var MyMap = DefineMap.extend({
+// 		foo: 'string'
+// 	});
+//
+// 	var map = new MyMap();
+//
+//
+// 	var stream = map.stream('foo');
+//
+// 	stream.onValue(function(ev){
+// 		QUnit.equal(expected, map.foo);
+// 	});
+//
+// 	expected = "obaid";
+// 	map.foo = "obaid";
+//
+// });
+//
+//
+// test('Create a stream on property that changes on change events', function() {
+// 	var expected = 0;
+// 	var MyMap = DefineMap.extend({
+// 		fooList: {
+// 			Type: DefineList.List,
+// 			value: []
+// 		}
+// 	});
+//
+// 	var map = new MyMap();
+//
+//
+// 	map.stream('fooList', 'length').onValue(function(ev){
+// 		QUnit.equal(expected, map.fooList.length);
+// 	});
+//
+// 	expected = 1;
+// 	map.fooList.push(1);
+//
+// 	expected = 0;
+// 	map.fooList.pop();
+//
+// });
+//
+// test('Create a stream on nested property', function() {
+// 	var expected = 1;
+// 	var MyMap = DefineMap.extend({
+// 		foo: {
+// 			value: {
+// 				bar: {
+// 					value: 1
+// 				}
+// 			}
+// 		}
+// 	});
+//
+// 	var map = new MyMap();
+//
+//
+// 	map.stream('foo', 'bar').onValue(function(ev){
+// 		QUnit.equal(map.foo.bar, expected);
+// 	});
+//
+// 	expected = 2;
+// 	map.foo.bar = 2;
+//
+// });
+//
+
+
+test('Stream behaviour test', function() {
+
+	var expected = '';
 
 	var MyMap = DefineMap.extend({
-		foo: 'string'
-	});
-
-	var map = new MyMap();
-
-
-	var stream = map.stream('foo');
-
-	stream.onValue(function(ev){
-		QUnit.equal(expected, map.foo);
-	});
-
-	expected = "obaid";
-	map.foo = "obaid";
-
-});
-
-
-test('Create a stream on property that changes on change events', function() {
-	var expected = 0;
-	var MyMap = DefineMap.extend({
-		fooList: {
-			Type: DefineList.List,
-			value: []
+		foo: 'string',
+		bar: { type: 'string', value: 'bar' },
+		baz: {
+			type: 'string',
+		    stream( stream ) {
+				debugger;
+				var fooStream = this.stream('foo');
+				fooStream.onValue(function(){
+					debugger;
+				});
+				return stream;//.merge(fooStream);
+		    },
+		    get(lastSetValue) {
+		      return `**${lastSetValue}**`;
+		    }
 		}
 	});
 
 	var map = new MyMap();
-
-
-	map.stream('fooList', 'length').onValue(function(ev){
-		QUnit.equal(expected, map.fooList.length);
+	debugger;
+	map.baz.onValue(function(evnt){
+		QUnit.equal(evnt.target.baz, expected);
 	});
 
-	expected = 1;
-	map.fooList.push(1);
-
-	expected = 0;
-	map.fooList.pop();
-
-});
-
-test('Create a stream on nested property', function() {
-	var expected = 1;
-	var MyMap = DefineMap.extend({
-		foo: {
-			value: {
-				bar: {
-					value: 1
-				}
-			}
-		}
-	});
-
-	var map = new MyMap();
-
-
-	map.stream('foo', 'bar').onValue(function(ev){
-		QUnit.equal(map.foo.bar, expected);
-	});
-
-	expected = 2;
-	map.foo.bar = 2;
-
+	map.foo = 'foo-1';
+	map.bar = 'new bar';
+	expected = 'baz-1';
+	map.baz = 'baz-1';
+	expected = 'baz-2';
+	map.baz = 'baz-2';
 });
